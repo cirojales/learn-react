ff6d1b81a59298dd4b6fde06ff154469c0532f78
cirojales <cbjales@gmail.com>
2022-10-20 11:59:49 +0000

    chore: upload the whole thing

---
+++ section01-react-facts-site/src/components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+import logo from "../../assets/Navbar/reactjs-icon.svg";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="nav--container">
+        <img src={logo} alt="" />
+        <span className="nav--react-facts">ReactFacts</span>
+      </div>
+      <span className="nav--text">React Course - Project 1</span>
+    </nav>
+  );
+}

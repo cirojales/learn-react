1846ba2b9af22bf8047fc076930d40736d6e8f3c
Ciro <cbjales@gmail.com>
2022-11-14 09:45:19 -0300

    chore: edit Entry component

--- section02-travel-journal/src/App.jsx
+++ section02-travel-journal/src/App.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Entry from "./components/Entry";
 import Header from "./components/Header";
+import data from "./data";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Entry />
+      {data &&
+        data.map((entry) => {
+          console.log(entry.googleMapsUrl);
+          return (
+            <Entry
+              title={entry.title}
+              location={entry.location}
+              googleMapsUrl={entry.googleMapsUrl}
+              startDate={entry.startDate}
+              endDate={entry.endDate}
+              description={entry.description}
+              imageUrl={entry.imageUrl}
+            />
+          );
+        })}
     </>
   );
 }
--- section02-travel-journal/src/components/Entry/index.jsx
+++ section02-travel-journal/src/components/Entry/index.jsx
@@ -2,22 +2,30 @@ import React from "react";
 import "./styles.css";
 import locationIcon from "../../assets/Entry/location-icon.svg";
 
-export default function Entry() {
+export default function Entry(props) {
   return (
     <div className="entry">
-      <img src="" alt="" />
-      <div>
-        <img
-          className="entry--location-icon"
-          src={locationIcon}
-          alt="location icon"
-        />
-        <span className="entry--location"></span>
-        <span className="entry--googleMaps">View on Google Maps</span>
+      <div className="entry--left-section">
+        <img className="entry--location-image" src={props.imageUrl} alt="" />
       </div>
-      <h2 className="entry--title"></h2>
-      <span className="startDate"></span>
-      <span className="endDate"></span>
+      <div className="entry--right-section">
+        <div className="wrapper">
+          <img
+            className="entry--location-icon"
+            src={locationIcon}
+            alt="location icon"
+          />
+          <span className="entry--location">{props.location}</span>
+          <a href={props.googleMapsUrl} className="entry--google-maps">
+            View on Google Maps
+          </a>
+        </div>
+        <h2 className="entry--title">{props.title}</h2>
+        <span className="entry--start-and-end-date">
+          {props.startDate} - {props.endDate}
+        </span>
+        <p className="entry--description">{props.description}</p>
+      </div>
     </div>
   );
 }

bcc11ed5df1c3235f123dbe79cc9c9842ca3e9d2
Ciro <cbjales@gmail.com>
2022-11-15 18:22:20 -0300

    chore: edit Header component

--- section03-meme-generator/src/components/Header/index.jsx
+++ section03-meme-generator/src/components/Header/index.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="header">
       <div className="left-wrapper">
         <img className="header--trollface" src={trollface} alt="trollface" />
-        <span className="header--title">Meme Generator</span>
+        <h2 className="header--title">Meme Generator</h2>
       </div>
       <div className="right-wrapper">
         <span className="header--text">React Course - Project 3</span>

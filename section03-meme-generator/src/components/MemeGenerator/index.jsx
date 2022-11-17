8573f9c59626054066d4fc6c7fbbabc35c5e6a23
Ciro <cbjales@gmail.com>
2022-11-17 17:55:54 -0300

    chore: edit MemeGenerator component

--- section03-meme-generator/src/components/MemeGenerator/index.jsx
+++ section03-meme-generator/src/components/MemeGenerator/index.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "./styles.css";
+import meme from "../../assets/meme.svg";
 
 export default function MemeGenerator() {
   return (
-    <form action="" className="form">
-      <div className="inputs-container">
-        <input type="text" className="line form--top-line" />
-        <input type="text" className="line form--bottom-line" />
-      </div>
-      {/* eslint-disable-next-line */}
-      <a className="link" href="">
-        Get a new meme image
-      </a>
-    </form>
+    <section className="meme-generator">
+      <form action="" className="form">
+        <div className="inputs-container">
+          <input type="text" className="line form--top-line" />
+          <input type="text" className="line form--bottom-line" />
+        </div>
+        {/* eslint-disable-next-line */}
+        <a className="link" href="">
+          Get a new meme image
+        </a>
+      </form>
+      <img className="meme" src={meme} alt="" />
+    </section>
   );
 }

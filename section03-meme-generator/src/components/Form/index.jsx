5f7a2a2339a18e4b376e527448a17eded3cf869d
Ciro <cbjales@gmail.com>
2022-11-17 17:11:15 -0300

    chore: edit Form component

--- section03-meme-generator/src/components/Form/index.jsx
+++ section03-meme-generator/src/components/Form/index.jsx
@@ -4,8 +4,13 @@ import "./styles.css";
 export default function Form() {
   return (
     <form action="" className="form">
-      <input type="text" className="line form--top-line" />
-      <input type="text" className="line form--bottom-line" />
+      <div className="inputs-container">
+        <input type="text" className="line form--top-line" />
+        <input type="text" className="line form--bottom-line" />
+      </div>
+      <a className="link" href="">
+        Get a new meme image
+      </a>
     </form>
   );
 }

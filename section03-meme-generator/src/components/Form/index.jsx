b563e4a80030f7a77ca1fd07af1b89fa499e3e13
Ciro <cbjales@gmail.com>
2022-11-17 16:24:30 -0300

    chore: set up Form component

---
+++ section03-meme-generator/src/components/Form/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function Form() {
+  return (
+    <form action="" className="form">
+      <input type="text" className="line form--top-line" />
+      <input type="text" className="line form--bottom-line" />
+    </form>
+  );
+}

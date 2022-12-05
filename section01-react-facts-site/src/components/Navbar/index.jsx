d4e793df802abb62a24c0c03bc8f2efbed61867b
Ciro <cbjales@gmail.com>
2022-12-05 19:19:00 -0300

    chore: edit Main and Navbar components

--- section01-react-facts-site/src/components/Navbar/index.jsx
+++ section01-react-facts-site/src/components/Navbar/index.jsx
@@ -4,15 +4,19 @@ import logo from "../../assets/Navbar/reactjs-icon.svg";
 
 export default function Navbar(props) {
   return (
-    <nav
-      onClick={props.toggleDarkMode}
-      className={props.darkMode ? "dark" : ""}
-    >
+    <nav className={props.darkMode ? "dark" : ""}>
       <div className="nav--container">
         <img src={logo} alt="" />
         <span className="nav--react-facts">ReactFacts</span>
       </div>
-      <span className="nav--text">React Course - Project 1</span>
+      {/* <span className="nav--text">React Course - Project 1</span> */}
+      <div className="toggler">
+        <p className="toggler--light">Light</p>
+        <div className="toggler--slider" onClick={props.toggleDarkMode}>
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
+      </div>
     </nav>
   );
 }
